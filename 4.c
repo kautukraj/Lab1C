6cea395641ff7f80acbbc651c13f0825cd19e2cb
Kautuk Raj <spamkraj@gmail.com>
2019-08-10 13:51:30 +0530

    Update 4.c

--- 4.c
+++ 4.c
@@ -6,6 +6,9 @@ int main ()
 double a,b,c;
 scanf ("%lf %lf %lf", &a, &b, &c);
 double r,r1,r2,real,i;
+ 
+ if (a!=0)
+ {
 if ((b*b - 4*a*c)  > 0 )
 {
  r1 = (-b + sqrt (b*b-4*a*c)) / 2*a ;
@@ -34,7 +37,7 @@ i = sqrt(4*a*c - b*b) / 2*a;
 printf( "%.2lf +  i %.2lf %.2lf -  i %.2lf\n", real, i, real, i); 
 
 }
-
+ }
 return 0;
 
 }
